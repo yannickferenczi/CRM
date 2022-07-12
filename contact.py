afb1046913eead02f626a6075a5ef664de98d704
yannickferenczi <yannick.ferenczi@outlook.com>
2022-07-12 19:58:49 +0200

    Update Contact

--- contact.py
+++ contact.py
@@ -1,12 +1,39 @@
-
+from tkinter import *
 
 class Contact:
-    def __init__(self):
-        self.first_name = ""
-        self.family_name = ""
-        self.phone_number = ""
-        self.address = ""
-        self.email_address = ""
+    def __init__(self, first_name, family_name, street, postcode, city, phone_number, email_address, row_number):
+        self.first_name = first_name.capitalize()
+        self.family_name = family_name.capitalize()
+        self.street = street
+        self.postcode = postcode
+        self.city = city.capitalize()
+        self.phone_number = phone_number
+        self.email_address = email_address
+        self.address = f"{self.street}, {self.postcode} {self.city}"
+        self.row_number = row_number
+
+    def pack_in_tk_frame(self, master_frame, row_number):
+        background = "#8866AA" if self.row_number % 2 == 0 else "#AA88CC"
+        row_frame = Frame(master_frame, bg=background)
+        row_frame.grid(row=self.row_number, column=0, columnspan=5)
+        first_name = Label(row_frame, text=self.first_name, bg=background, width=15)
+        first_name.grid(row=self.row_number, column=0)
+        family_name = Label(row_frame, text=self.family_name, bg=background, width=15)
+        family_name.grid(row=self.row_number, column=1)
+        address = Label(row_frame, text=self.address, bg=background, width=45)
+        address.grid(row=row_number, column=2)
+        phone_number = Label(row_frame, text=self.phone_number, bg=background, width=15)
+        phone_number.grid(row=self.row_number, column=3)
+        email_address = Label(row_frame, text=self.email_address, bg=background, width=45)
+        email_address.grid(row=self.row_number, column=4)
+        
+        modification_icone = PhotoImage(file="modification.png")
+        modification_button = Button(row_frame, image=modification_icone, bg="#EEEEEE", width=30, command=self.contact_modification)
+        modification_button.grid(row=0, column=5, padx=5)
+
+        delete_icone = PhotoImage(file="trash.png")
+        delete_button = Button(row_frame, image=delete_icone, bg="#EEEEEE", width=30, command=self.delete_contact)
+        delete_button.grid(row=0, column=6, padx=5)
 
     def contact_modification(self):
         pass
--- crm.py
+++ crm.py
@@ -1,16 +1,22 @@
 from tkinter import *
 import sqlite3
-from turtle import bgcolor
 
+from contact import Contact
 
 class CRM:
     def __init__(self) -> None:
         self.search = "all"
         self.root = Tk()
-        self.root.geometry("1000x600")
+        self.root.title("CRM")
+        self.root.geometry("1250x600")
         self.root.configure(bg="#88CCFF")
         
-        self.contact_frame = LabelFrame(self.root, text=self.search)
+        self.settings_frame = LabelFrame(self.root, text="Options")
+        self.settings_frame.pack()
+        self.research_label = Label(self.settings_frame, text="Search : ")
+        self.research_label.grid(row=0, column=0)
+
+        self.contact_frame = LabelFrame(self.root, text=self.search, bg="#FF88FF")
         self.contact_frame.pack()
 
         self.display_contact()
@@ -19,38 +25,35 @@ class CRM:
         self.root.mainloop()
     
     def display_contact(self):
+        # Create the header of the display
+        first_name_header = Label(self.contact_frame, text="First name", bg="#FF88FF", width=15)
+        first_name_header.grid(row=0, column=0)
+        family_name_header = Label(self.contact_frame, text="Family name", bg="#FF88FF", width=15)
+        family_name_header.grid(row=0, column=1)
+        address_header = Label(self.contact_frame, text="Address", bg="#FF88FF", width=45)
+        address_header.grid(row=0, column=2)
+        phone_number_header = Label(self.contact_frame, text="Phone number", bg="#FF88FF", width=15)
+        phone_number_header.grid(row=0, column=3)
+        email_address_header = Label(self.contact_frame, text="Email", bg="#FF88FF", width=45)
+        email_address_header.grid(row=0, column=4)
+        space_for_icones = Label(self.contact_frame, bg="#FF88FF", width=5)
+        space_for_icones.grid(row=0, column=5, columnspan=2)
+
+        # For each contact in DB, create an instance of Contact and store it in a dictionnary
+        dict_of_contacts = {}
+
         self.sql_conn = sqlite3.connect("contact_data.db")
         self.curs = self.sql_conn.cursor()
-        first_name_header = Label(self.contact_frame, text="First name")
-        first_name_header.grid(row=0, column=0)
-        family_name_header = Label(self.contact_frame, text="Family name")
-        family_name_header.grid(row=0, column=1)
-        address_header = Label(self.contact_frame, text="Address")
-        address_header.grid(row=0, column=2)
-        phone_number_header = Label(self.contact_frame, text="Phone number")
-        phone_number_header.grid(row=0, column=3)
-        email_address_header = Label(self.contact_frame, text="Email")
-        email_address_header.grid(row=0, column=4)
 
         row_number = 1
-        background = "#8866AA" if row_number % 2 == 0 else "#AA88CC"
         for row in self.curs.execute("SELECT * FROM contacts"):
-            row_frame = Frame(self.contact_frame, bg=background)
-            row_frame.grid(row=row_number, column=0, columnspan=5)
-            first_name = Label(self.contact_frame, text=row[0], bg=background)
-            first_name.grid(row=row_number, column=0)
-            family_name = Label(self.contact_frame, text=row[1], bg=background)
-            family_name.grid(row=row_number, column=1)
-            address = Label(self.contact_frame, text=f"{row[2]}, {row[3]} {row[4]}", bg=background)
-            address.grid(row=row_number, column=2)
-            phone_number = Label(self.contact_frame, text=row[5], bg=background)
-            phone_number.grid(row=row_number, column=3)
-            email_address = Label(self.contact_frame, text=row[6], bg=background)
-            email_address.grid(row=row_number, column=4)
-
+            dict_of_contacts[str(row_number)] = Contact(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row_number)
             row_number += 1
         self.sql_conn.close()
 
+        # Display the Contact instances 
+        for personne in dict_of_contacts.values():
+            personne.pack_in_tk_frame(self.contact_frame, row_number)
 
 
 if __name__ == "__main__":
